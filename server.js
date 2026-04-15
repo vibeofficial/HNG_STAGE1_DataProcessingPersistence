3d470dc4308bd582f2de8bb3067102a608dd6130
vibeofficial <ichioguchristopher98@gmail.com>
2026-04-15 03:51:51 +0100

    done

--- server.js
+++ server.js
@@ -2,24 +2,24 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import connectDB from "./database/mongoose.js";
+
 import express from "express";
 import profileRouter from "./routes/profile.js";
 import cors from "cors";
-import morgan from "morgan";
-import serverless from "serverless-http";
-
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(cors({ origin: "*" }));
-app.use(morgan("combined"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
   return res.status(200).json({ status: "ok" });
 });
 
-app.use("/api", profileRouter);
+app.use('/api', profileRouter);
 
 connectDB();
 
-export default serverless(app);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
