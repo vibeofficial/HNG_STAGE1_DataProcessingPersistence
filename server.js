fbe4202ca2dc3a4c689a9631766f81ab9f139183
vibeofficial <ichioguchristopher98@gmail.com>
2026-04-15 03:46:50 +0100

    done

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ import connectDB from "./database/mongoose.js";
 import express from "express";
 import profileRouter from "./routes/profile.js";
 import cors from "cors";
+import morgan from "morgan";
 import serverless from "serverless-http";
 
 const app = express();
 
 app.use(cors({ origin: "*" }));
+app.use(morgan("combined"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
