5acb286be320bf1a1d360e7b3b957e0e25943f23
vibeofficial <ichioguchristopher98@gmail.com>
2026-04-15 03:05:24 +0100

    done

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,17 +1,23 @@
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+import mongoose from "mongoose";
+
 const DB = process.env.MONGODB_URI;
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+};
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(DB);
-    console.log("Database connected");
-  } catch (error) {
-    console.error("Database connection failed:", error.message);
-    process.exit(1);
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(DB).then((mongoose) => mongoose);
   }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 export default connectDB;
--- server.js
+++ server.js
@@ -2,12 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import connectDB from "./database/mongoose.js";
-
 import express from "express";
 import profileRouter from "./routes/profile.js";
 import cors from "cors";
+import serverless from "serverless-http";
+
 const app = express();
-const PORT = process.env.PORT;
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
@@ -16,10 +16,8 @@ app.get("/", (req, res) => {
   return res.status(200).json({ status: "ok" });
 });
 
-app.use('/api', profileRouter);
+app.use("/api", profileRouter);
 
 connectDB();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default serverless(app);
